f4aa79168051d459e5f1859cca6179bc00ff8773
yusrizaltomo <98932520+yusrizaltomo@users.noreply.github.com>
2022-05-26 21:17:43 +0700

    Delete pencatat_tugas_kuliah directory

--- pencatat_tugas_kuliah/tampil/tampil_smt.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-	include "koneksi.php";
-
-	// ambil data semester lalu urutkan berdasarkan semesternya
-	$tampil_smt = mysqli_query($koneksi, "SELECT * FROM semester ORDER BY semester");
-	$banyak_smt = mysqli_num_rows($tampil_smt);
-
- ?>
